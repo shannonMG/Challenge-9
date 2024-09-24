460a10f77f488ab085856977a6ea342c1bb844d8
ShannonMG <shannonmastrogiovanni@gmail.com>
2024-09-23 21:54:30 -0400

    fdsfew

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,17 +7,13 @@ dotenv.config(); // -this is what is actually reading the .env file
 // Import the routes
 import weatherRoutes from './routes/api/weatherRoutes.js';
 import htmlRoutes from './routes/htmlRoutes.js';
-
-
-const __filename = fileURLToPath(import.meta.url);  // Get current file path
-const __dirname = path.dirname(__filename);   
+   
 
 const app = express();
-
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
-app.use(express.static(path.join(__dirname, "../client"))); //uses express' static middleware to server static files from the ./client/dist
+app.use(express.static("../client/dist")); //uses express' static middleware to server static files from the ./client/dist
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
 app.use(express.json()); // parses incoming requests with JSON and makes data available in req.body
